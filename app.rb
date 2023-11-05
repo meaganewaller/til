832bc1a38cfec357bd91428ac1dfbec49f12ccef
Meagan Waller <meagan.waller@testdouble.com>
2023-11-05 16:24:18 -0500

    ✨ Add ability to add a learning from til page

--- app.rb
+++ app.rb
@@ -1,38 +1,69 @@
-require "rack"
-require "slim"
+require 'rack'
+require 'slim'
 
 module Til
   class App
-    def initialize
-      @app = Rack::Builder.new do
-        run lambda { |_env|
-          [
-            200,
-            {"Content-Type" => "text/html"},
-            [Til::App.render_template("index")]
-          ]
-        }
-
-        map '/til' do
-          run lambda { |_env|
-            [
-              200,
-              { 'Content-Type' => 'text/html' },
-              [Til::App.render_template('til')]
-            ]
-          }
-        end
-      end.to_app
+    def initialize(db)
+      @db = db
     end
 
     def call(env)
-      @app.call(env)
+      request = Rack::Request.new(env)
+      case request.path
+      when '/'
+        index_response
+      when '/til'
+        if request.request_method == 'GET'
+          view_tils
+        elsif request.request_method == 'POST'
+          create_til(env)
+        end
+      else
+        render_not_found
+      end
     end
 
-    def self.render_template(template_name)
-      template_path = File.join(File.dirname(__FILE__), "views", "#{template_name}.slim")
-      template = File.read(template_path)
-      Slim::Template.new { template }.render(self)
+    private
+
+    def render_view(view_name, locals = {})
+      template = File.read("views/#{view_name}.slim")
+      rendered = Slim::Template.new { template }.render(self, locals)
+      [200, { 'Content-Type' => 'text/html' }, [rendered]]
+    end
+
+    def index_response
+      render_view('index')
+    end
+
+    def render_not_found
+      template = File.read('views/not_found.slim')
+      rendered = Slim::Template.new { template }.render(self)
+      [404, { 'Content-Type' => 'text/html' }, [rendered]]
+    end
+
+    def view_tils
+      tils = retrieve_tils || []
+
+      render_view('til', { tils: })
+    end
+
+    def create_til(env)
+      request = Rack::Request.new(env)
+      content = request.params['content']
+
+      insert_til(content)
+
+      view_tils
+    end
+
+    def insert_til(content)
+      @db[:learnings].insert(content:)
+    end
+
+    def retrieve_tils
+      all_learnings = @db.from(:learnings)
+      all_learnings.exclude(content: nil)
+      all_learnings.order(Sequel.desc(:id))
     end
   end
 end
--- config.ru
+++ config.ru
@@ -2,7 +2,10 @@
 
 require 'rack'
 require_relative 'app'
+require 'sequel'
 
 use Rack::Static, urls: ['/assets'], root: 'public'
 
-run Til::App.new
+db = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://127.0.0.1/til_development')
+
+run Til::App.new(db)
